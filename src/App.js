1e96054ddeec73a29814f3086ce15a4e13229d41
Fan Yiwei <nf140901@gmail.com>
2023-05-26 17:25:34 +0800

    initial Commit, structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import NavBar from './Components/NavBar'
+import Banner from './Components/Banner'
+import Skills from './Components/Skills'
+import Projects from './Components/Projects'
+import Connect from './Components/Connect'
+import Footer from './Components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Connect />
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;
