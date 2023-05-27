911105dfbe17dd1fdc7cde671c327203c390548c
Fan Yiwei <nf140901@gmail.com>
2023-05-27 16:30:58 +0800

    added projects componnet, changde the layouts

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './index.css'
 
+import projects from '../../data/project_data'
+
 export default function Projects() {
   return (
-    <div id='projects'>Projects</div>
+    <div id='projects'>
+      <section className="projects-com" id="projects">
+        <div className="container">
+          <p className="big-title">Projects</p>
+          <p className="big-desc">Here are some of my works</p>
+          <div className="choose-box">
+            {projects.map((item, index) => (
+              <p key={index}>{item.name}</p>
+            ))}
+          </div>
+          <div className="show-box">
+            {projects.map((e, i) => (
+              <div className="show-img" key={i} onClick={() => { window.open(e.github, '_blank') }}>
+                <img src={e.img} alt="img load error" />
+                <div className="mask-box">
+                  <div className="name">{e.name}</div>
+                  <div className="desc">{e.desc}</div>
+                </div>
+              </div>
+            ))
+            }
+          </div>
+        </div>
+      </section>
+
+    </div>
   )
 }
---
+++ src/data/project_data.js
@@ -0,0 +1,33 @@
+import Bake from '../Projects_Images/baker.png'
+import Weather from '../Projects_Images/weather.png'
+import Eshooper from '../Projects_Images/e-shopper.png'
+import BikeMap from '../Projects_Images/bike-parking.png'
+
+const projects = [
+  {
+    name: 'Bake of Bakers',
+    desc: 'A bakery commercial website is a website created for a company with the purpose of promoting its products or services, engaging with customers, and driving business growth, made with react.',
+    img: Bake,
+    github: 'https://github.com/imnkywf/bake-off-bakers'
+  },
+  {
+    name: 'Weather Forecast App',
+    desc: 'The global weather forecast app, developed using React and Redux, retrieves weather information for various cities worldwide by leveraging AJAX to fetch data from openweather API.',
+    img: Weather,
+    github: 'https://github.com/imnkywf/weather-forecast-app'
+  },
+  {
+    name: 'E-Shopper',
+    desc: 'E-Shopper is an e-commerce website that offers a platform for users to discover and purchase new products. The website is built using a combination of technologies, including React, Redux, AJAX, and a Node.js server.',
+    img: Eshooper,
+    github: 'https://github.com/imnkywf/e-shopper'
+  },
+  {
+    name: 'Bike Parking Availablilty Map',
+    desc: "The Bike Parking Availability Map is a web app that allows users to check the availability of bike parking spaces in Singapore. It's built with React, Redux, Node.js, AJAX, and Leaflet for a seamless user experience.",
+    img: BikeMap,
+    github: 'https://github.com/technical-zebra/parking_availbility_map'
+  },
+]
+
+export default projects
